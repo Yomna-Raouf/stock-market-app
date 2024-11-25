fdea1202e4aa6086c0a9d9936c7309a654e9cb49
Yomna-Raouf <yomnaraouf99@gmail.com>
2024-11-25 22:05:16 +0200

    feat: add infinite scrolling and api states

--- src/views/tickers/api/getTickers.ts
+++ src/views/tickers/api/getTickers.ts
@@ -1,4 +1,4 @@
-import { UseQueryOptions, useQuery } from 'react-query';
+import { useInfiniteQuery, UseInfiniteQueryOptions } from 'react-query';
 
 import { axiosClient } from '@/api/index';
 
@@ -7,23 +7,31 @@ import type { AxiosError } from 'axios';
 
 import { API_KEY } from '@/constants';
 
-export const getTickers = async ({ limit = 100 }: { limit?: number }): Promise<{ results: Ticker[] }> => {
-  const { data } = await axiosClient.get<{ results: Ticker[] }>(
-    `/reference/tickers?active=true&limit=${limit}&apiKey=${API_KEY}`,
+export const getTickers = async ({
+  limit = 15,
+  next_page_url,
+}: {
+  limit?: number;
+  next_page_url?: string;
+}): Promise<{ results: Ticker[]; next_url: string }> => {
+  const { data } = await axiosClient.get<{ results: Ticker[]; next_url: string }>(
+    `/reference/tickers?active=true&limit=${limit}&apiKey=${API_KEY}${next_page_url ? `&cursor=${next_page_url.split('cursor=')[1]}` : ''}`,
   );
 
-  return { results: data.results };
+  return { results: data.results, next_url: data.next_url };
 };
 
 type UseGetExitModelingOptions = {
   limit?: number;
-  config?: Partial<UseQueryOptions<{ results: Ticker[] }, AxiosError<{ message: string }>>>;
+  next_page_url?: string;
+  config?: Partial<UseInfiniteQueryOptions<{ results: Ticker[]; next_url: string }, AxiosError<{ message: string }>>>;
 };
 
-export const useGetTickers = ({ limit, config }: UseGetExitModelingOptions) => {
-  return useQuery({
-    queryKey: ['get-tickers', limit],
-    queryFn: () => getTickers({ limit }),
+export const useGetTickers = ({ limit, next_page_url, config }: UseGetExitModelingOptions) => {
+  return useInfiniteQuery({
+    queryKey: ['get-tickers'],
+    queryFn: () => getTickers({ limit, next_page_url }),
+    getNextPageParam: (lastPage) => lastPage.next_url,
     ...config,
   });
 };
