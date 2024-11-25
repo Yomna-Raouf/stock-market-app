ee3173633f0c8fc8fb2174e23e75a41113a1196c
Yomna-Raouf <yomnaraouf99@gmail.com>
2024-11-25 18:14:45 +0200

    fix: fix failing test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,21 @@
 import { describe, it } from 'vitest';
 import { render } from '@testing-library/react';
-import App from './App';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { MemoryRouter } from 'react-router';
+import AppRoutes from './routes';
+
+const queryClient = new QueryClient();
 
 describe('App', () => {
   it('renders the App component', () => {
-    render(<App />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <AppRoutes />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
   });
 });
