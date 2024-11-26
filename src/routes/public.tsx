cd0e5336451ef1bd6f1eb00245243fd2ecabeb6e
Yomna-Raouf <yomnaraouf99@gmail.com>
2024-11-26 09:15:10 +0200

    feat: create home page

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -3,12 +3,17 @@ import type { RouteObject } from 'react-router';
 import MainLayout from '@/components/Layouts/MainLayout/MainLayout';
 
 import { TickersRoutes } from '@/views/tickers/routes';
+import { HomeRoutes } from '@/views/routes';
 
 export const PublicRoutes: RouteObject[] = [
   {
     path: '/*',
     element: <MainLayout />,
     children: [
+      {
+        path: '',
+        element: <HomeRoutes />,
+      },
       {
         path: 'tickers/*',
         element: <TickersRoutes />,
---
+++ src/views/home/Home.tsx
@@ -0,0 +1,13 @@
+import NasdaqIcon from '@/components/Icons/NasdaqIcon';
+
+import styles from './styles.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <NasdaqIcon />
+    </div>
+  );
+};
+
+export default Home;
